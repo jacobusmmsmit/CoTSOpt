8fc4c06f77b984b9840c1f2738b0c77e17c63e34
jacobusmmsmit <57063134+jacobusmmsmit@users.noreply.github.com>
2022-04-29 14:28:38 +0100

    Add train/test split to Bayesian optimisation

--- scripts/real_test.jl
+++ scripts/real_test.jl
@@ -9,17 +9,21 @@ using StatsPlots
 default(fontfamily="Computer Modern")
 df = CSV.read("data/export_test.csv", DataFrame)
 all_data = Array(df[:, 2:end])
-true_data = all_data[:, 1]
-port_data = [all_data[:, i] for i in 2:size(all_data[:, 2:end], 2)]
+train_ndays = floor(Int, size(all_data, 1) / 1.3)
+test_ndays = size(all_data, 1) - train_ndays
+
+train_data = all_data[1:train_ndays, 1]
+train_port_data = [all_data[1:train_ndays, i] for i in 2:size(all_data[:, 2:end], 2)]
+test_data = all_data[train_ndays+1:train_ndays+test_ndays, 1]
+test_port_data = [all_data[train_ndays+1:train_ndays+test_ndays, i] for i in 2:size(all_data[:, 2:end], 2)]
 
 nports = length(port_data)
-ndays = length(true_data)
-p = nports, ndays
+p = nports, train_ndays
 
 @model function export_model(exports, port_data, p)
     nports, ndays = p
     # Set priors
-    port_included ~ filldist(Beta(0.01, 0.01), nports)
+    port_included ~ filldist(Beta(1 / 300, 1 / 300), nports)
     σ ~ InverseGamma(2, 3)
 
     # Predict given sample from prior
@@ -30,58 +34,63 @@ p = nports, ndays
     exports ~ MvNormal(predicted, σ)
 end
 
-model = export_model(true_data, port_data, p)
-chain = sample(model, NUTS(), 5000)
+model = export_model(train_data, train_port_data, p)
+chain = sample(model, NUTS(), 1000)
 
-function prediction_plot(newdata, chain, p, nsamples=10)
+function prediction_plot(train_data, test_data, chain, p; nsamples=10)
     nports, _ = p
     chain_array = Array(chain) # Pull the parameter samples
-    ndays_newdata = size(newdata[1])
+
+    ndays_train_data = length(train_data[1])
+    ndays_test_data = length(test_data[1])
+    println(ndays_train_data)
     prediction_plot = plot()
-    predicted = zeros(ndays_newdata)
+
+    train_predicted = zeros(ndays_train_data)
     for _ in 1:nsamples
-        predicted .= 0
+        train_predicted .= 0
         port_included_sample = chain_array[rand(1:size(chain_array, 1)), 1:nports]
         for i in 1:nports
-            predicted .+= newdata[i] .* port_included_sample[i]
+            train_predicted .+= train_data[i] .* port_included_sample[i]
         end
-        plot!(predicted, label="", colour=:grey, alpha=5 / nsamples, lw=2)
+        plot!(1:ndays_train_data, train_predicted, label="", colour=:grey, alpha=5 / nsamples, lw=2)
     end
-    # quantiles = map(x -> quantile(x, [0.25, 0.75]), eachcol(chain_array))[1:end-1]
-    # quantiles_long = first.(quantiles), last.(quantiles)
-    # pred_quantile_upper = zeros(ndays_newdata)
-    # pred_quantile_lower = zeros(ndays_newdata)
-    # for i in 1:nports
-    #     pred_quantile_lower .+= newdata[i] .* quantiles_long[1][i]
-    #     pred_quantile_upper .+= newdata[i] .* quantiles_long[2][i]
-    # end
-
     port_included_mean = vec(mean(chain_array, dims=1)[:, 1:end-1])
-    predicted .= 0
+    train_predicted .= 0
     for i in 1:nports
-        predicted .+= newdata[i] .* port_included_mean[i]
+        train_predicted .+= train_data[i] .* port_included_mean[i]
     end
-    # pred_quantile_lower .= predicted .- pred_quantile_lower
-    # pred_quantile_upper .-= predicted
-    plot!(predicted, colour=:red, alpha=0.5, lw=2, label="Prediction")
-    # true_newdata = reduce(hcat, newdata) * (1:nports .∈ Ref(true_ports))
-    # plot!(true_newdata, colour=:black, label="True data", ls=:dash, legend=:topleft)
-    # my_correlation = round(cor(true_newdata, predicted), sigdigits=3)
-    # plot!(title="Predicted Timeseries (Corr = $my_correlation)", xlabel="Day", ylabel="Value")
+    plot!(1:ndays_train_data, train_predicted, colour=:blue, alpha=0.5, lw=2, label="Training Prediction")
+
+    test_predicted = zeros(ndays_test_data)
+    for _ in 1:nsamples
+        test_predicted .= 0
+        port_included_sample = chain_array[rand(1:size(chain_array, 1)), 1:nports]
+        for i in 1:nports
+            test_predicted .+= test_data[i] .* port_included_sample[i]
+        end
+        plot!(ndays_train_data+1:ndays_train_data+ndays_test_data, test_predicted, label="", colour=:grey, alpha=5 / nsamples, lw=2)
+    end
+    port_included_mean = vec(mean(chain_array, dims=1)[:, 1:end-1])
+    test_predicted .= 0
+    for i in 1:nports
+        test_predicted .+= test_data[i] .* port_included_mean[i]
+    end
+    plot!(ndays_train_data+1:ndays_train_data+ndays_test_data, test_predicted, colour=:red, alpha=0.5, lw=2, label="Test Prediction")
     return prediction_plot
 end
 
 begin
-    prediction_plot(port_data, chain, p, 400)
-    plot!(true_data, colour=:black, label="True Data", ls=:dash)
-    pred_ports = port_included_mean = vec(mean(Array(chain), dims=1)[:, 1:end-1])
-    predicted = zeros(ndays)
+    prediction_plot(train_port_data, test_port_data, chain, p, nsamples=400)
+    plot!(all_data[:, 1], colour=:black, label="True Data", ls=:dash)
+    pred_ports = vec(mean(Array(chain), dims=1)[:, 1:end-1])
+    predicted = zeros(test_ndays)
     for i in 1:nports
-        predicted .+= port_data[i] .* pred_ports[i]
+        predicted .+= test_port_data[i] .* pred_ports[i]
     end
-    c = round(cor(predicted, true_data), sigdigits=3)
-    plot!(title="Predicted Timeseries (Correlation = $c)", xlabel="Month", ylabel="Exports in Tons")
+    c = round(cor(predicted, test_data), sigdigits=3)
+    plot!(title="Predicted Timeseries (Test Correlation = $c)", xlabel="Month", ylabel="Exports in Tons")
 end
-savefig("figures/realdata_example.png")
 
-bar(1:length(pred_ports), pred_ports,)
+savefig("figures/realdata_example_traintest.png")
+bar(1:length(pred_ports), pred_ports, label="")
